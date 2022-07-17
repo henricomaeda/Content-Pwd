6aafeaee397cbfcb9ff64255d42bc84b06de60a0
Henrico Pelozato Maeda <henrico.maeda@gmail.com>
2022-07-16 22:31:11 -0300

    Atualizando arquivos

--- Content Pwd/Program.cs
+++ Content Pwd/Program.cs
@@ -16,7 +16,7 @@ namespace Content_Pwd
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FrmMain());
         }
     }
 }
