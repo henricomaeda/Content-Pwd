5ffac6e436e81d7213eccc8598cf16ac60e71fae
Henrico Pelozato Maeda <henrico.maeda@gmail.com>
2022-07-16 22:39:07 -0300

    Gerando arquivos

--- Content Pwd/FrmMain.Designer.cs
+++ Content Pwd/FrmMain.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMain));
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AccessibleDescription = "Put a passcode on your content";
+            this.AccessibleName = "Content Pwd";
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(608, 450);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Content Pwd";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
